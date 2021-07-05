6d7ea1554173132d2716ed2907952e0155b0e2d7
Martin <mnegro91@gmail.com>
2021-07-05 15:20:22 -0300

    Creados modelos DB

---
+++ api/src/models/Genre.js
@@ -0,0 +1,15 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('genre', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+};
