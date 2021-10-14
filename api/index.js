3a2e79effa4795f4c75528e2a234926c7e49f972
Martín Negro <mnegro91@gmail.com>
2021-10-14 17:18:31 -0300

    port config

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const port = PORT.process.env || 3001
+const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
