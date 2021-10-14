04979bcd71ba184aef2ee1394ac9ab0507fa5f7e
Martín Negro <mnegro91@gmail.com>
2021-10-14 17:11:54 -0300

    port config

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const port = PORT.process || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
