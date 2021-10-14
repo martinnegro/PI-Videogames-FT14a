4978e2ff5d816f9b6b07a8eade5239ebc5f5cb82
Martín Negro <mnegro91@gmail.com>
2021-10-14 17:05:53 -0300

    deploy config

--- api/src/db.js
+++ api/src/db.js
@@ -2,16 +2,23 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+const { DATABASE_URL } = process.env;
 const { dbLoad } = require('./dbLoad');
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = new Sequelize(`${DATABASE_URL}`, 
+  {
+    logging: false,
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -43,7 +50,7 @@ Videogame.belongsToMany(Genre, { through: 'videogamegenre' });
 Genre.belongsToMany(Videogame, { through: 'videogamegenre' });
 
 // Se pasan los modelos como parámtros ya que no se pueden hacer exports circulares
-// dbLoad(Videogame, Genre, Platform);
+dbLoad(Videogame, Genre, Platform);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
